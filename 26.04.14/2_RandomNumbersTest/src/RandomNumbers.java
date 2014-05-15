4a975b8df7945a12b034a07c654f8c128bd2432c
Tanya <tanya_b_94@mail.ru>
2014-05-15 23:30:36 +0400

    Homework

--- 26.04.14/2_RandomNumbersTest/src/RandomNumbers.java
+++ 26.04.14/2_RandomNumbersTest/src/RandomNumbers.java
@@ -10,17 +10,19 @@ import java.util.Random;
 public class RandomNumbers {
     PrintWriter writer;
     HashMap<Integer, Integer> result = new HashMap<Integer, Integer>();
+    int max, min;
 
-    public RandomNumbers(PrintWriter writer){
+    public RandomNumbers(PrintWriter writer) {
         this.writer = writer;
     }
 
     public void run() throws IOException {
-        for(int maxValue = 10; maxValue <= 100; maxValue++){ // для каждого k от 10 до 100
+        for (int maxValue = 10; maxValue <= 100; maxValue++) { // для каждого k от 10 до 100
             writer.println("#" + maxValue + ":");
-            for (int kol = 1000; kol < 10000000; kol*=10) {
+            for (int kol = 1000; kol < 10000000; kol *= 10) {
                 generateRandomNumbers(maxValue, kol);
-                double d = dCount(maxValue, kol);
+                findMaxAndMin(kol);
+                double d = (double) (max - min) / (double) kol;
                 writer.println(kol + " - " + d);
                 result.clear();
             }
@@ -29,40 +31,28 @@ public class RandomNumbers {
     }
 
     private void generateRandomNumbers(int maxValue, int kol) {
-        for (int i = 1; i<=kol; i++){
+        for (int i = 1; i <= kol; i++) {
             Random rnd = new Random();
-            int currentNumber = rnd.nextInt(maxValue)+1;
-            result.put(i,currentNumber);
+            int currentNumber = rnd.nextInt(maxValue) + 1;
+            int currentCount = 0;
+            if (result.containsKey(currentNumber)) {
+                currentCount = result.get(currentNumber);
+            }
+            result.put(currentNumber, currentCount + 1);
         }
     }
 
-    private double dCount(int maxValue, int kol) {
-        int[] amount = new int[maxValue+1];
-        for (Map.Entry<Integer,Integer> en: result.entrySet()) {
-            amount[en.getValue()]++;
-        }
-        int max = maxKol(amount);
-        int min = minKol(amount);
-        return (double)(max-min)/(double)kol;
-    }
-
-    private int maxKol(int[] amount) {
-        int max = amount[1];
-        for (int i = 2; i<amount.length; i++){
-            if (amount[i] > max){
-                max = amount[i];
+    private void findMaxAndMin(int maxKol) {
+        max = 0;
+        min = maxKol;
+        for (Map.Entry<Integer, Integer> en : result.entrySet()) {
+            int currentCount = en.getValue();
+            if (currentCount > max) {
+                max = currentCount;
+            }
+            if (currentCount < min) {
+                min = currentCount;
             }
         }
-        return max;
-    }
-
-    private int minKol(int[] amount) {
-        int min = amount[1];
-        for (int i = 2; i<amount.length; i++){
-            if (amount[i] < min){
-                min = amount[i];
-            }
-        }
-        return min;
     }
 }
